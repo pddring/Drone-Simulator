8a1c19bd0d9f0eb608607456ef08ba094b5f90cf
Pete Dring <pddring@googlemail.com>
2018-12-01 17:50:21 +0000

    Added keyboard controls (WASD, UP)

---
+++ Assets/RotateWithKeys.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateWithKeys : MonoBehaviour {
+    /// <summary>
+    /// Key to press to increase rotation
+    /// </summary>
+    public KeyCode increaseKey;
+
+    /// <summary>
+    /// Key to press to decrease rotation
+    /// </summary>
+    public KeyCode decreaseKey;
+
+    public KeyCode resetKey;
+
+    public float maxRotation;
+    public float minRotation;
+
+    /// <summary>
+    /// Angle to apply when the key is pressed
+    /// </summary>
+    public float angle;
+
+    private float currentRotation = 0;
+
+    /// <summary>
+    /// Axis to rotate around
+    /// </summary>
+    public Vector3 axis;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Reset()
+    {
+        transform.Rotate(axis, -currentRotation);
+        currentRotation = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        if(Input.GetKey(increaseKey))
+        {
+            if(angle + currentRotation < maxRotation  )
+            {
+                transform.Rotate(axis, angle);
+                currentRotation += angle;
+            }
+            
+        }
+
+        if (Input.GetKey(decreaseKey))
+        {
+            if(currentRotation - angle > minRotation)
+            {
+                transform.Rotate(axis, -angle);
+                currentRotation -= angle;
+            } 
+        }
+
+        if(Input.GetKeyDown(resetKey))
+        {
+            Reset();
+        }
+
+
+    }
+}
