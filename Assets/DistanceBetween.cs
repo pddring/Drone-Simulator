9554ea6c5598955a7c8802318fe5f05d174f3742
Pete Dring <pddring@googlemail.com>
2019-03-24 21:59:17 +0000

    Added waypoints

---
+++ Assets/DistanceBetween.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DistanceBetween : MonoBehaviour {
+    public Transform movingObject;
+    public Transform targetObject;
+    public Text altitudeLabel;
+    public WayPoint waypoint;
+
+
+    private Text label;
+	// Use this for initialization
+	void Start () {
+        label = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 difference = movingObject.position - targetObject.position;
+        float altitude = difference.y;
+        difference.y = 0;
+        float distance =  Mathf.Sqrt (difference.sqrMagnitude);
+
+        if(altitudeLabel )
+        {
+            altitudeLabel.text = altitude.ToString("F1");
+        }
+
+        if(waypoint)
+        {
+            waypoint.updateDistance(altitude, distance);
+        }
+        label.text =distance.ToString("F1");
+	}
+}
---
+++ Assets/WayPoint.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WayPoint : MonoBehaviour {
+    private float altitude;
+    private float distance;
+    private int score = 0;
+    public Text scoreLabel;
+
+    public float minDistance = 5;
+    public float minAltitude = 1;
+
+    public Vector3[] positions;
+    private int positionIndex = -1;
+    
+    public void updateDistance(float altitude, float distance)
+    {
+        this.altitude = altitude;
+        this.distance = distance;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (altitude < minAltitude  && distance < minDistance)
+        {
+            positionIndex = (positionIndex + 1) % positions.Length;
+            transform.SetPositionAndRotation(positions[positionIndex], Quaternion.identity);
+            score++;
+            if (scoreLabel)
+                scoreLabel.text = score.ToString();
+        } 
+	}
+
+    
+}
