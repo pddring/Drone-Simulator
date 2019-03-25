cadbfdf56c861838db752ccbb64e363af018fe72
Pete Dring <pddring@googlemail.com>
2019-03-25 07:22:26 +0000

    Added reset (r)

---
+++ Assets/Reset.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset : MonoBehaviour {
+    public KeyCode resetKey;
+    public Vector3 resetPosition;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(resetKey))
+        {
+            transform.SetPositionAndRotation(resetPosition, Quaternion.identity);
+            Rigidbody rb = GetComponent<Rigidbody>();
+            rb.velocity = Vector3.zero;
+        }
+	}
+}
